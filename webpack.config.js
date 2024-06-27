58291f6376364f0a8b4effa612c54e8a2eb57f5c
superpuper777 <superfevrik@gmail.com>
2024-06-27 20:54:15 +0300

    add images and fonts

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,11 @@ export default {
           },
         },
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
