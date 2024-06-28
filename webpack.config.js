bf3ccafc409526b1521739a0b4963a2684fb2466
superpuper777 <superfevrik@gmail.com>
2024-06-28 11:29:30 +0300

    change the configuration file, add markings and styles

--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,19 @@ export default {
   module: {
     rules: [
       {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-
         type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[hash][ext][query]',
+        },
       },
       {
         test: /\.js$/,
@@ -38,6 +44,9 @@ export default {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
 
         type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[hash][ext][query]',
+        },
       },
     ],
   },
