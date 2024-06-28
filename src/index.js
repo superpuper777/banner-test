3bea52d76a94b9b82f9e8dfdbe4388ff8e922c95
superpuper777 <superfevrik@gmail.com>
2024-06-28 23:06:04 +0300

    make a basic layout, add a translation, setup middlewares

--- src/index.js
+++ src/index.js
@@ -1,13 +1,84 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const languages = ['en', 'fr', 'es', 'de', 'it', 'ru'];
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const getLanguage = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const lang = urlParams.get('lang');
 
-  return element;
-}
+  if (lang && languages.includes(lang)) {
+    return lang;
+  }
 
-document.body.appendChild(component());
+  const systemLang = navigator.language.slice(0, 2);
+  if (languages.includes(systemLang)) {
+    return systemLang;
+  }
+
+  return 'en';
+};
+
+const loadLanguage = async (lang) => {
+  try {
+    const response = await fetch(`assets/locales/${lang}.json`);
+    const data = await response.json();
+    useTranslations(data);
+  } catch (error) {
+    console.error('Error loading:', error);
+  }
+};
+
+const interpolateTemplate = (template, variables) => {
+  return template.replace(/{{(.*?)}}/g, (match, key) => variables[key.trim()]);
+};
+
+const useTranslations = (strings) => {
+  const yearlyPrice = '$39.99';
+  const perWeekYearlyPrice = '$0.48';
+  const perWeekWeeklyPrice = '$6.99';
+
+  document.querySelector('.header__title').innerHTML =
+    strings['Get Unlimited <br>Access'];
+
+  const cards = document.querySelectorAll('.card');
+  cards[0].querySelector('.card__text').innerHTML =
+    strings['Unlimited Art <br>Creation'];
+  cards[1].querySelector('.card__text').innerHTML =
+    strings['Exclusive <br>Styles'];
+  cards[2].querySelector('.card__text').innerHTML =
+    strings['Magic Avatars <br>With 20% Off'];
+
+  document.querySelector('.offer').textContent = strings['BEST OFFER'];
+
+  const selectors = document.querySelectorAll('.selector');
+  selectors[0].querySelector('.selector__title').textContent =
+    strings['YEARLY ACCESS'];
+  selectors[0].querySelector('.selector__text').textContent =
+    interpolateTemplate(strings['Just {{price}} per year'], {
+      price: yearlyPrice,
+    });
+  selectors[0].querySelector('.selector__price .selector__text').innerHTML =
+    interpolateTemplate(strings['{{price}} <br>per week'], {
+      price: perWeekYearlyPrice,
+    });
+
+  selectors[1].querySelector('.selector__title').textContent =
+    strings['WEEKLY ACCESS'];
+  selectors[1].querySelector('.selector__text').innerHTML = interpolateTemplate(
+    strings['{{price}} <br>per week'],
+    {
+      price: perWeekWeeklyPrice,
+    }
+  );
+
+  document.querySelector('.button').textContent = strings['Continue'];
+  document.querySelector('.footer__link.terms').textContent =
+    strings['Terms of Use'];
+  document.querySelector('.footer__link.privacy').textContent =
+    strings['Privacy Policy'];
+  document.querySelector('.footer__link.restore').textContent =
+    strings['Restore'];
+};
+
+const language = getLanguage();
+loadLanguage(language);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -54,12 +55,45 @@ export default {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/assets/locales', to: 'assets/locales' }],
+    }),
   ],
+  // devServer: {
+  //   static: {
+  //     directory: path.join(__dirname, 'dist'),
+  //   },
+  //   compress: true,
+  //   port: 9000,
+  // },
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
     compress: true,
     port: 9000,
+    open: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    setupMiddlewares: (middlewares, devServer) => {
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined');
+      }
+
+      devServer.app.use((req, res, next) => {
+        const url = new URL(req.url, `http://${req.headers.host}`);
+        if (!url.searchParams.get('lang')) {
+          const redirectUrl = new URL(req.url, `http://${req.headers.host}`);
+          redirectUrl.searchParams.set('lang', 'en');
+          res.writeHead(302, { Location: redirectUrl.toString() });
+          res.end();
+        } else {
+          next();
+        }
+      });
+
+      return middlewares;
+    },
   },
 };
