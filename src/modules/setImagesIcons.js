38c432abfd02c66930a60183ba3af65bc3bdafeb
superpuper777 <superfevrik@gmail.com>
2024-07-12 20:29:13 +0300

    update project with new images

--- src/modules/setImagesIcons.js
+++ src/modules/setImagesIcons.js
@@ -16,7 +16,7 @@ export const setImagesIcons = () => {
     }
 
     if (folder) {
-      srcset = `${src.replace('png', '1.5@.svg')} 1.5x, `;
+      srcset = `${src.replace('.png', '@1.5x.png')} 1.5x, `;
     }
     srcset += `${src.replace('.png', '@2x.png')} 2x, ${src.replace(
       '.png',
